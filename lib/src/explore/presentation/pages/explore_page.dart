d4f6fe61180628ce249efb4ee033a7936f719581
Samuel Kow Paintsil <smashbros54@gmail.com>
2021-07-31 04:10:09 +0000

    ✨ add nearby services

--- lib/src/explore/presentation/pages/explore_page.dart
+++ lib/src/explore/presentation/pages/explore_page.dart
@@ -1,5 +1,9 @@
 import 'package:flutter/material.dart';
 import 'package:google_maps_flutter/google_maps_flutter.dart';
+import 'package:home_x_client/src/explore/domain/entities/nearby_service.dart';
+import 'package:home_x_client/src/explore/presentation/bloc/explore_bloc.dart';
+
+import '../../../../injection_container.dart';
 
 class ExplorePage extends StatefulWidget {
   const ExplorePage({Key? key}) : super(key: key);
@@ -10,21 +14,25 @@ class ExplorePage extends StatefulWidget {
 
 class _ExplorePageState extends State<ExplorePage> {
   late GoogleMapController _controller;
+  final _bloc = sl<ExploreBloc>();
 
   final LatLng _center = const LatLng(28.535517, 77.391029);
   final Map<String, Marker> _markers = {};
 
   Future<void> onMapCreated(GoogleMapController controller) async {
     _controller = controller;
+    List<NearbyService> nearbyServices = await _bloc.getNearbyServices();
 
     setState(() {
       _markers.clear();
-      final marker = Marker(
-          markerId: MarkerId(''),
-          position: LatLng(1.256, 354.56),
-          onTap: () => Navigator.of(context).pushNamed('/serviceDetails'),
-          infoWindow: InfoWindow());
-      _markers[''] = marker;
+      for (final nearbyService in nearbyServices) {
+        final marker = Marker(
+            markerId: MarkerId(nearbyService.key),
+            position: LatLng(nearbyService.latitude, nearbyService.longitude),
+            onTap: () => Navigator.of(context).pushNamed('/serviceDetails'),
+            infoWindow: InfoWindow(title: nearbyService.serviceName));
+        _markers[nearbyService.key] = marker;
+      }
     });
   }
 
