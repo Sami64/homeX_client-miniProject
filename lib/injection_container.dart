df4ebcf67c87e65a94001a80501b93fd80449335
Samuel Kow Paintsil <smashbros54@gmail.com>
2021-07-31 04:05:24 +0000

    🎉 add explore to injection

--- lib/injection_container.dart
+++ lib/injection_container.dart
@@ -1,6 +1,7 @@
 import 'package:get_it/get_it.dart';
 import 'package:hive/hive.dart';
 import 'package:home_x_client/core/user/user_dependency_injection.dart';
+import 'package:home_x_client/src/explore/explore_dependency_injection.dart';
 import 'package:home_x_client/src/home/home_dependency_injection.dart';
 import 'package:home_x_client/src/login/login_dependency_injection.dart';
 import 'package:home_x_client/src/service_details/service_details_dependency_injection.dart';
@@ -31,6 +32,7 @@ void initFeatures() {
   initServiceDetails();
   initLogin();
   initSignup();
+  initExplore();
 }
 
 void initExternal() {
